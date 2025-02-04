11ef6bd290ec26815c76ebb1621c795ea525c066
Illia Ivshyn <korabel7777@gmail.com>
2025-02-04 11:04:14 +0200

    third step done

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,11 @@
+from brain_games.cli import welcome_user
+
 def greeting():
     print('Welcome to the Brain Games!')
 
 def main():
     greeting()
+    welcome_user()
 
 
 if __name__ == "__main__":
