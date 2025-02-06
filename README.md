6028c6980c429958c49aded2ef772054f67301e5
Illia Ivshyn <korabel7777@gmail.com>
2025-02-07 00:00:52 +0200

    5th step done

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # python-project-lvl1
 [![Maintainability](https://api.codeclimate.com/v1/badges/7345376ddb5cacc7e5d5/maintainability)](https://codeclimate.com/github/ksfrules/python-project-lvl1/maintainability)
 [![Test Coverage](https://api.codeclimate.com/v1/badges/7345376ddb5cacc7e5d5/test_coverage)](https://codeclimate.com/github/ksfrules/python-project-lvl1/test_coverage)
+<a href="https://asciinema.org/a/haJOEcSS9kk3pO9CG5bxMiuKh" target="_blank"><img src="https://asciinema.org/a/haJOEcSS9kk3pO9CG5bxMiuKh.svg" /></a>
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,56 @@
+import prompt
+from random import randint
+
+
+def greeting():
+    print('Welcome to the Brain Games!')
+
+def welcome_user():
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    return name
+
+def game_rules():
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+
+def question():
+    rand_int = randint(1, 1000)
+    print(f'Question: {rand_int}')
+    return rand_int
+
+def answer():
+    answer = prompt.string('Your answer: ')
+    return answer
+
+def check(answer, rand_int):
+    if answer == "yes" and rand_int % 2 == 0:
+        return True
+    elif answer == "no" and rand_int % 2 == 1:
+        return True
+    else:
+        return False
+    
+def main():
+    greeting()
+    user_name = welcome_user()
+    game_rules()
+    counter = 0
+    while counter < 3:
+        rand_int =  question()
+        user_answer = answer()
+        if check(user_answer, rand_int):
+            print('Correct')
+            counter += 1
+        else:
+            if rand_int % 2 == 0:
+                print(f"'{user_answer}' is wrong answer :(.Correct answer was 'yes'")
+            else:
+                print(f"'{user_answer}' is wrong answer :(.Correct answer was 'no'")
+            print(f"Let's try again, {user_name}!")
+            break
+    if counter == 3:
+        print(f"Congratulations, {user_name}!")
+
+
+if __name__ == "__main__":
+    main()
